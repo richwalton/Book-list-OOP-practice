3c40c9cba88d56e7669ca4ce41cc04fdbf5ad853
Rich Walton <sbtsun@yahoo.com>
2020-07-21 22:50:16 -0700

    added new version file re-written using "Classes" or ES6

---
+++ booklist-es6.js
@@ -0,0 +1,104 @@
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+class UI {
+   addBookToList(book) {
+    const list = document.getElementById('book-list');
+    // console.log(list);
+    const row = document.createElement('tr');
+    
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class='delete'>X</a></td>`;
+    
+    list.appendChild(row);
+    }
+//}
+
+   showAlert(message, className) {
+    //Create div
+    const div = document.createElement('div');
+    //Add Class
+    div.className = `alert ${className}`;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+
+    // Get parent
+    const container = document.querySelector('.container');
+
+    const form = document.querySelector('#book-form');
+    //Insert alert
+    container.insertBefore(div, form);
+
+    // Timeout after 3 sec
+    setTimeout(function(){
+        document.querySelector('.alert').remove();
+    }, 3000);
+   }
+
+   deleteBook(target) {
+    target.parentElement.parentElement.remove(); 
+   }
+
+   clearFields() {
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+   }
+}
+
+// Event Listeners for add a book
+document.getElementById('book-form').addEventListener('submit', function(e){
+    //Get form values
+    const title = document.getElementById('title').value,
+          author = document.getElementById('author').value,
+          isbn = document.getElementById('isbn').value;
+
+    //instantiate Book
+    const book = new Book(title, author, isbn);
+
+    //Instantiate UI
+    const ui = new UI();
+
+    console.log(ui);
+
+    // Validate
+    if(title === '' | author === '' | isbn === ''){
+        //Error alert
+        ui.showAlert('Please fill in all fields', 'error')
+    } else {
+
+    //Add book to list
+    ui.addBookToList(book);
+
+    // Show success
+    ui.showAlert('Book Added!', 'success');
+
+    //Clear fields
+    ui.clearFields();
+
+    }
+    e.preventDefault();
+});
+
+// Event listener for delete
+document.getElementById('book-list').addEventListener('click', function(e){
+    // Instantiate UI
+    const ui = new UI();
+    
+    if(e.target.className === 'delete') {
+    
+    ui.deleteBook(e.target);
+
+    // Show message
+    ui.showAlert('Book Removed', 'success');
+    }
+    e.preventDefault();
+});
